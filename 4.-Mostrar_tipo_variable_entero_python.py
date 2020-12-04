6581fd821b3ef5bcd1293530fb8653a0eac0bb48
Emilio Carcaño Bringas <emiliocarcano@uninaciones.edu.mx>
2020-12-03 19:36:35 -0600

    Add files via upload

---
+++ 4.-Mostrar_tipo_variable_entero_python.py
@@ -0,0 +1,12 @@
+'''
+Este programa muestra el tipado dinámico,
+'''
+
+print("Este programa muestra el valor de una variable y de qué tipo es")
+numero = 7
+print("El valor de la variable es:")
+print(numero)
+print(type(numero))
+print("Este programa fue escrito por Emilio Carcaño Bringas")
+
+#Este programa fue escrito por Emilio Carcaño Bringas
